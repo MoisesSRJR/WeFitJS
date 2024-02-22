e122b176ff5c417ff2f91319b452184eb6f07bcc
MoisesSRJR <moises.sampaiojr@outlook.com>
2024-02-22 12:46:47 -0300

    first commit

---
+++ js-assets/wefit-test/js/main.js
@@ -0,0 +1,96 @@
+const changeMenuToHorizontal = () => {
+  const divMenu = document.querySelector('.btn-group-vertical')
+  divMenu.setAttribute('class', 'btn-group-horizontal')
+  divMenu.setAttribute('aria-label', 'Horizontal button group')
+}
+
+const changeButtonHeader = () => {
+  const buttonHeader = document.querySelector('a.btn.btn-primary.btn-lg')
+  buttonHeader.classList.remove('btn-primary')
+  buttonHeader.classList.add('btn-success')
+}
+
+const changeHeaderStyles = () => {
+  const header = document.querySelector('.jumbotron')
+  header.classList.add('text-right')
+  header.style.cssText = `
+    background-color: var(--gray);
+    color: var(--white);
+  `
+}
+
+const changeHeaderBox = () => {
+  changeButtonHeader()
+  changeHeaderStyles()
+}
+
+const addTitleDataset = (nodeList) => {
+  nodeList.forEach((title) => {
+    const nearCard = title.closest('div.card');
+    nearCard.dataset['title'] = title.textContent;
+  })
+}
+
+const changeColorButtonCard = (dataTitle) => {
+  const button = document.querySelector(`[data-title="${dataTitle}"] .btn-primary`);
+  button.classList.remove('btn-primary');
+  button.classList.add('btn-success');
+}
+
+const putBefore = (nodeList, from, to) => {
+  const parent = nodeList[to].offsetParent.parentElement
+  parent.insertBefore(
+    nodeList[from].offsetParent,
+    nodeList[to].offsetParent,
+  )
+}
+
+const alterCardOrder = () => {
+  const cards = document.querySelectorAll('.card');
+  putBefore(cards, 3, 0)
+  putBefore(cards, 2, 1)
+}
+
+const changeCards = () => {
+  alterCardOrder()
+  
+  const cardsTitle = document.querySelectorAll('.card-title');
+  addTitleDataset(cardsTitle)
+  changeColorButtonCard('Animais')
+}
+
+const newLi = ({ text, isActive = false }) => {
+  const contentText = document.createTextNode(text)
+  
+  const li = document.createElement('li')
+  li.classList.add('list-group-item')
+
+  if (isActive) {
+    li.classList.add('active')
+  }
+
+  li.appendChild(contentText)
+
+  return li
+}
+
+const removeActiveClass = () => {
+  const liActive = document.querySelector('li.active')
+  liActive.classList.remove('active')
+}
+
+const changeList = () => {
+  removeActiveClass()
+
+  const ul = document.querySelector('ul.list-group')
+  const fourthLi = newLi({ text: 'Quarto item', isActive: true })
+  const fifthLi = newLi({ text: 'Quinto item' })
+
+  ul.appendChild(fourthLi)
+  ul.appendChild(fifthLi)
+}
+
+changeMenuToHorizontal()
+changeHeaderBox()
+changeCards()
+changeList()
